3a14e8c64cd54a34f8e7d8fc2b6c8be84f957499
mr-omid <terminator444411@gmail.com>
2024-06-09 11:14:22 +0330

    update gitignore , read file js

--- read_file.js
+++ read_file.js
@@ -1,8 +1,34 @@
 var fs = require('fs')
 
 var images_name = fs.readdirSync('./images/')
+var new_images_name = new Array();
 
-fs.writeFile('images.json',JSON.stringify(images_name),(err)=>{
+var has_prefix_number = false;
+
+images_name.forEach((single_image)=>{
+    var name = single_image.split('.');
+
+    if(name[0] == (0+name[0]).toString()){
+        has_prefix_number = true;
+    }
+
+    new_images_name.push(parseInt(name[0]));
+});
+
+new_images_name.sort((a,b)=> a - b);
+console.log(new_images_name);
+
+new_images_name.map((current,index)=>{
+    if(current.toString().length > 1 && has_prefix_number){
+         new_images_name[index] = 0 + current.toString() + '.jpg';
+    }else{
+        new_images_name[index] = current.toString() + '.jpg';
+    }
+})
+
+console.log(new_images_name);
+
+fs.writeFile('images.json',JSON.stringify(new_images_name),(err)=>{
     if(err){
         console.log(err);
     }else{
