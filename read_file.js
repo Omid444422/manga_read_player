59c89559c054406cd35e7b747fd3aadcd89bd003
mr_omid <terminator444411@gmail.com>
2024-07-21 12:02:30 +0330

    fix prefix files bug

--- read_file.js
+++ read_file.js
@@ -8,18 +8,19 @@ var has_prefix_number = false;
 images_name.forEach((single_image)=>{
     var name = single_image.split('.');
 
-    if(name[0] == (0+name[0]).toString()){
-        has_prefix_number = true;
+    if(name[0][0] == '0'){
+        has_prefix_number = true
     }
 
     new_images_name.push(parseInt(name[0]));
 });
 
+
 new_images_name.sort((a,b)=> a - b);
 console.log(new_images_name);
 
 new_images_name.map((current,index)=>{
-    if(current.toString().length > 1 && has_prefix_number){
+    if(current.toString().length == 1 && has_prefix_number){
          new_images_name[index] = 0 + current.toString() + '.jpg';
     }else{
         new_images_name[index] = current.toString() + '.jpg';
